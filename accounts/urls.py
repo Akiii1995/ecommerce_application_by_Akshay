21ee87d44fc8204cbcebb0caca9c290a515df12d
akiii1995 <akshaypatil9519@gmail.com>
2024-12-02 14:57:28 +0530

    user registration funcationality

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 from accounts import views
 urlpatterns = [
     path('admin/',admin.site.urls),
-    path('login/',views.login_page,name='login')
+    path('login/',views.login_page,name='login'),
+    path('register/',views.register_page,name='register'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
-
+from django.contrib import messages
 # Create your views here.
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
 
 def login_page(request):
     return render(request,'accounts/login.html')
+
+def register_page(request):
+    # function for signup user
+    if request.method=='POST':
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user_obj = User.objects.filter(username=email)
+        if user_obj.exists():
+            messages.warning(request,'Email is all ready taken.')
+            return HttpResponseRedirect(request.path_info)
+        user_obj = User.objects.create(first_name=first_name, last_name=last_name,email=email,username=email)
+        user_obj.set_password(password)
+        user_obj.save()
+        messages.success(request,'An Email has been sent on your mail')
+        return HttpResponseRedirect(request.path_info)
+    return render(request,'accounts/register.html')
